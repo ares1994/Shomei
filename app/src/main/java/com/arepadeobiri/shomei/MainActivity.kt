920fecfdbc929d6e00dd4656be55bcea283351eb
Ares Obiri <arepade.obiri@fundall.io>
2020-10-25 19:01:10 +0100

    Next Commit

--- app/src/main/java/com/arepadeobiri/shomei/MainActivity.kt
+++ app/src/main/java/com/arepadeobiri/shomei/MainActivity.kt
@@ -3,6 +3,8 @@ package com.arepadeobiri.shomei
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//Commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
