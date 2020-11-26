1e4fe121de2ba4b2c1b17a49dc88abe3c26e5a80
Ares Obiri <arepade.obiri@fundall.io>
2020-11-26 11:41:34 +0100

    More fixes

--- app/src/main/java/com/arepadeobiri/shomei/MainActivity.kt
+++ app/src/main/java/com/arepadeobiri/shomei/MainActivity.kt
@@ -5,29 +5,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.arepadeobiri.shomeiview.ShomeiView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        val shomeiView = findViewById<ShomeiView>(R.id.shomeiView)
-//
-//        var count = 1
-//
-//
-//        findViewById<Button>(R.id.main_button).apply {
-//            setOnClickListener {
-//
+
+        val shomeiView = findViewById<ShomeiView>(R.id.shomeiView)
+
+        var count = 1
+
+
+        findViewById<Button>(R.id.main_button).apply {
+
+
+            shomeiView.setFrameType(ShomeiView.FrameType.DirectOppositesTopBottom)
+            shomeiView.setDrawColor(Color.parseColor("#4DE897"))
+
+
+
+            setOnClickListener {
+
 //                Toast.makeText(this.context, "Button clicked", Toast.LENGTH_LONG).show()
 //                shomeiView.setFrameType(ShomeiView.FrameType.OneSide)
-//                shomeiView.setCanvasColor(Color.parseColor("#016938"))
-//                shomeiView.invalidate()
-//
-//            }
-//        }
-//
+//                shomeiView.setDrawColor(Color.parseColor("#016938"))
+
+
+
+
+
+
+                when(count){
+                    1->{
+
+                    }
+                    2->{
+                    }
+                    3->{
+                    }
+                    4->{
+//                        shomeiView.setCanvasColor(Color.parseColor("#7EC0F9"))
+                    }
+
+                }
+
+
+                shomeiView.getUri(externalCacheDir)
+
+
+                count++
+
+
+            }
+        }
+
 
     }
 }
